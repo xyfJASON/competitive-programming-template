a4408df76a8a45ca10e3d9abdefbfa5ecf0757dc
xyfJASON <190110230@stu.hit.edu.cn>
2020-10-05 21:55:29 +0800

    Add FFT&NTT

--- 模板/数学 Mathematics/多项式 Polynomial/code/快速傅立叶变换 Fast Fourier Transform.cpp
+++ 模板/数学 Mathematics/多项式 Polynomial/code/快速傅立叶变换 Fast Fourier Transform.cpp
@@ -1,77 +1,63 @@
-#include<cmath>
-#include<cstdio>
-#include<algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-const int N = 3000005;
 const double PI = acos(-1);
-
-inline int read(){
-	int x = 0, fl = 1;
-	char ch = getchar();
-	while(ch < '0' || ch > '9'){
-		if(ch == '-')   fl = -1;
-		ch = getchar();
-	}
-	while(ch >= '0' && ch <= '9'){
-		x = (x << 1) + (x << 3) + ch - '0';
-		ch = getchar();
-	}
-	return x * fl;
-}
 
 struct Complex{
 	double real, imag;
-	Complex():real(0),imag(0){}
-	Complex(double r, double i){ real = r; imag = i; }
-	Complex operator + (Complex &A){ return Complex(real+A.real,imag+A.imag); }
-	Complex operator - (Complex &A){ return Complex(real-A.real,imag-A.imag); }
-	Complex operator * (Complex &A){ return Complex(real*A.real-imag*A.imag,real*A.imag+imag*A.real); }
+	Complex(){ real = imag = 0; }
+	Complex(double r, double i){ real = r, imag = i; }
+	Complex operator + (Complex &A){ return Complex(real+A.real, imag+A.imag); }
+	Complex operator - (Complex &A){ return Complex(real-A.real, imag-A.imag); }
+	Complex operator * (Complex &A){ return Complex(real*A.real-imag*A.imag, real*A.imag+imag*A.real); }
 };
 
-int lenf, leng, rev[N];
-Complex f[N], g[N];
-
-void fft(Complex A[], int n, int flag){ // flag == 1: fft; flag == -1: ifft (inverse fft)
-	for(int i = 0; i < n; i++)
-		if(i < rev[i])
-			swap(A[i], A[rev[i]]);
-	for(int m = 2; m <= n; m <<= 1){
-		Complex wm( cos(2*PI/m), flag * sin(2*PI/m) ); // mth unit root
-		for(int k = 0; k < n; k += m){
-			Complex w(1, 0);
-			for(int j = 0; j < m / 2; j++){
-				Complex t = w * A[k+j+m/2], u = A[k+j];
-				A[k+j] = u + t;
-				A[k+j+m/2] = u - t;
-				w = w * wm;
+namespace FFT{
+	int n;
+	vector<int> rev;
+	inline void preprocess(int _n, int _m){
+		int cntBit = 0;
+		for(n = 1; n <= _n + _m; n <<= 1, cntBit++);
+		// n == 2^cntBit is a upper bound of lenf+leng
+		rev.resize(n);
+		for(int i = 0; i < n; i++)
+			rev[i] = (rev[i>>1]>>1) | ((i&1) << (cntBit-1));
+			// rev[k] is bit-reversal permutation of k
+	}
+	inline void fft(vector<Complex> &A, int flag){
+		// flag == 1: DFT; flag == -1: IDFT
+		A.resize(n);
+		for(int i = 0; i < n; i++) if(i < rev[i]) swap(A[i], A[rev[i]]);
+		for(int m = 2; m <= n; m <<= 1){
+			Complex wm(cos(2*PI/m), flag * sin(2*PI/m));
+			for(int k = 0; k < n; k += m){
+				Complex w(1, 0);
+				for(int j = 0; j < m / 2; j++){
+					Complex t = w * A[k+j+m/2], u = A[k+j];
+					A[k+j] = u + t, A[k+j+m/2] = u - t;
+					w = w * wm;
+				}
 			}
 		}
+		if(flag == -1)
+			for(int i = 0; i < n; i++)
+				A[i].real /= n;
 	}
-	if(flag == -1)
-		for(int i = 0; i < n; i++)
-			A[i].real /= n;
 }
 
 int main(){
-	lenf = read(); leng = read();
-	for(int i = 0; i <= lenf; i++)
-		f[i].real = read();
-	for(int i = 0; i <= leng; i++)
-		g[i].real = read();
-	int n = 1, cntBit = 0;
-	while(n <= lenf + leng){
-		n <<= 1;
-		cntBit++;
-	}                           // n == 2^cntBit is a upper bound of lenf+leng
-	for(int i = 0; i < n; i++)  // rev[k] is bit-reversal permutation of k
-		rev[i] = ( rev[i>>1] >> 1 ) | ( (i&1) << (cntBit-1) );
-	fft(f, n, 1);               // f[] used to be coefficients, now they're point-values
-	fft(g, n, 1);               // g[] used to be coefficients, now they're point-values
-	for(int i = 0; i < n; i++)  f[i] = f[i] * g[i];
-	fft(f, n, -1);              // f[] used to be point-values, now they're coefficients
-	for(int i = 0; i <= lenf + leng; i++)
+	int n, m;
+	scanf("%d%d", &n, &m);
+	vector<Complex> f(n+1), g(m+1);
+	for(int i = 0; i <= n; i++)	scanf("%lf", &f[i].real);
+	for(int i = 0; i <= m; i++)	scanf("%lf", &g[i].real);
+	FFT::preprocess(n, m);
+	FFT::fft(f, 1); // f used to be coefficients, now they're point-values
+	FFT::fft(g, 1); // g used to be coefficients, now they're point-values
+	for(int i = 0; i < FFT::n; i++)	f[i] = f[i] * g[i];
+	FFT::fft(f, -1); // f used to be point-values, now they're coefficients
+	for(int i = 0; i < n + m + 1; i++)
 		printf("%d ", (int)(f[i].real+0.5));
 	return 0;
 }
---
+++ 模板/数学 Mathematics/多项式 Polynomial/code/快速数论变换 Number Theory Transform.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+
+const LL MOD = 998244353;
+const LL G = 3;
+const LL invG = 332748118;
+
+inline LL fpow(LL bs, LL idx){
+	LL res = 1;
+	while(idx){
+		if(idx & 1)	(res *= bs) %= MOD;
+		(bs *= bs) %= MOD;
+		idx >>= 1;
+	}
+	return res;
+}
+
+namespace NTT{
+	int n;
+	vector<int> rev;
+	inline void preprocess(int _n, int _m){
+		int cntBit = 0;
+		for(n = 1; n <= _n + _m; n <<= 1, cntBit++);
+		// n == 2^cntBit is a upper bound of lenf+leng
+		rev.resize(n);
+		for(int i = 0; i < n; i++)
+			rev[i] = (rev[i>>1]>>1) | ((i&1) << (cntBit-1));
+			// rev[k] is bit-reversal permutation of k
+	}
+	inline void ntt(vector<LL> &A, int flag){
+		// flag == 1: NTT; flag == -1: INTT
+		A.resize(n);
+		for(int i = 0; i < n; i++) if(i < rev[i]) swap(A[i], A[rev[i]]);
+		for(int m = 2; m <= n; m <<= 1){
+			LL wm = flag == 1 ? fpow(G, (MOD-1)/m) : fpow(invG, (MOD-1)/m);
+			for(int k = 0; k < n; k += m){
+				LL w = 1;
+				for(int j = 0; j < m / 2; j++){
+					LL t = w * A[k+j+m/2] % MOD, u = A[k+j];
+					A[k+j] = (u + t) % MOD;
+					A[k+j+m/2] = (u - t + MOD) % MOD;
+					w = w * wm % MOD;
+				}
+			}
+		}
+		if(flag == -1){
+			LL inv = fpow(n, MOD-2);
+			for(int i = 0; i < n; i++)
+				(A[i] *= inv) %= MOD;
+		}
+	}
+}
+
+int main(){
+	int n, m;
+	scanf("%d%d", &n, &m);
+	vector<LL> f(n+1), g(m+1);
+	for(int i = 0; i <= n; i++)	scanf("%lld", &f[i]);
+	for(int i = 0; i <= m; i++)	scanf("%lld", &g[i]);
+	NTT::preprocess(n, m);
+	NTT::ntt(f, 1); // f used to be coefficients, now they're point-values
+	NTT::ntt(g, 1); // g used to be coefficients, now they're point-values
+	for(int i = 0; i < NTT::n; i++)	f[i] = f[i] * g[i];
+	NTT::ntt(f, -1); // f used to be point-values, now they're coefficients
+	for(int i = 0; i < n + m + 1; i++)
+		printf("%lld ", f[i]);
+	return 0;
+}
